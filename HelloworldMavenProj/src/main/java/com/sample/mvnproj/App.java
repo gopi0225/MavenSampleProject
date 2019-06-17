1f9c6db4808195c85e684cf25eb956948578bf47
Gopinath <gopi.atp@gmail.com>
2019-06-17 22:34:57 +0530

    sample test

--- HelloworldMavenProj/src/main/java/com/sample/mvnproj/App.java
+++ HelloworldMavenProj/src/main/java/com/sample/mvnproj/App.java
@@ -8,6 +8,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Log.info("This is sample information logs");
+        Log.info("This is sample information logs test");
     }
 }
