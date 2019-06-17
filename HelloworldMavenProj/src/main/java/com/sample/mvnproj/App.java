0137ad7583ef3a96dd0d41801f481ad367b90314
Gopinath <gopi.atp@gmail.com>
2019-06-17 22:59:28 +0530

    Added the current date

--- HelloworldMavenProj/src/main/java/com/sample/mvnproj/App.java
+++ HelloworldMavenProj/src/main/java/com/sample/mvnproj/App.java
@@ -1,5 +1,8 @@
 package com.sample.mvnproj;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class App 
@@ -9,5 +12,13 @@ public class App
     {
         System.out.println( "Hello World!" );
         Log.info("This is sample information logs test");
+        
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        String date = simpleDateFormat.format(new Date());
+        System.out.println(date);
+        
+        Log.info("Current date:"+date);
     }
 }
